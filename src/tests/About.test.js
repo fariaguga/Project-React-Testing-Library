2ed896ca0a515b84a8dadd1974b3bb443addbee5
Gustavo <fariaguga@hotmail.com>
2021-11-05 16:40:55 -0200

    Arruma lint  Requisito 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação. ', 
+  () => {
+    it('O primeiro Link deve possuir o texto "Home" ', () => {
+
+    });
+  });
